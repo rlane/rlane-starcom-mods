3fe551929a5265e8a14809e7f248c0f7f97da0ad
Rich Lane <lanerl@gmail.com>
2020-01-13 21:05:20 -0800

    initial commit

---
+++ Drift/Drift.cs
@@ -0,0 +1,25 @@
+﻿using Harmony;
+using UnityEngine;
+using System.Reflection;
+using System;
+
+namespace Drift
+{
+    [HarmonyPatch(typeof(Ship), "RefreshBuild")]
+    internal class Ship_RefreshBuild
+    {
+        private static void Postfix(Ship __instance)
+        {
+            __instance.Rigid.drag = 0.1f;
+        }
+    }
+
+    static class Main
+    {
+        static void Load()
+        {
+            var harmony = HarmonyInstance.Create("com.github.rlane.rlane-starcom-mods.drift");
+            harmony.PatchAll(Assembly.GetExecutingAssembly());
+        }
+    }
+}
---
+++ Drift/Properties/AssemblyInfo.cs
@@ -0,0 +1,16 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Drift")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Drift")]
+[assembly: AssemblyCopyright("Copyright © Rich Lane 2020")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+[assembly: ComVisible(false)]
+[assembly: Guid("0669a623-77c6-4844-ad1a-2666147e9ea0")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
